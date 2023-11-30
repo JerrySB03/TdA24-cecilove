d718bae0569259b7f63d328990648365088cb30f
stepan bilek <bilek.st.2022@skola.ssps.cz>
2023-11-30 12:32:43 +0100

    feat: phase 1

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>try 1</h1>
+<h1>Hello TdA</h1>
 <p class="text-4xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi a dignissimos doloribus! Officiis fugiat necessitatibus sit soluta repudiandae, quod sed nostrum qui accusamus aliquid a ut veritatis magnam porro quos!</p>
