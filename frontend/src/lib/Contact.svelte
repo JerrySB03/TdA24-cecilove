e6f9cde8d93f348df9d1c04e3c601a770cf767cc
stepan bilek <bilek.st.2022@skola.ssps.cz>
2023-12-23 18:26:44 +0100

    haaaaaaaaaaaaaaa

---
+++ frontend/src/lib/Contact.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    interface Contact{
+        telephone_numbers : String[];
+        emails : String[];
+    }
+   export let contact : Contact = {telephone_numbers: [], emails : []};
+
+
+</script>
+
+<div>
+    {#each contact.telephone_numbers as number}
+        <p>{number}</p>
+    {/each}
+    {#each contact.emails as mail}
+        <p>{mail}</p>
+    {/each}
+</div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   mode : 'jit',
-  purge : ['./src/**/*.svelte']
+  purge : ['./src/**/*.svelte'],
+
+  theme: {
+    extend: {
+      colors:{
+        jet: "#333333",
+        skyblue: "#74C7D3",
+        sunglow: "#FECB2E",
+        prussianBlue: "00384D"
+      }
+    },
+  },
 };
